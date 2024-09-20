b8fc4463824c8e83a112711305740eacb3c5228a
erfanlabbafi <erfanlab2000@gmail.com>
2024-09-21 00:24:27 +0330

    start learn typeScript simplilearn

--- start-learn-TypeScript.js
+++ start-learn-TypeScript.js
@@ -1,72 +1,48 @@
 // // lesson 1 learning for variable
-
 // var age : number = 45;
-
 // let employname  : string = 'erfan';
-
 // const num: number = 100;
-
 // number
 //  we can add variable : Number , String , Boolean , ...
 // let frist: number = 123; //number 
 // let second: number = 0x67CF; //hexadecimal
 // let third: number = 0o455;// decimal
 // let fourth: number = 0b110101;// binery
-
 // lesson 2 : learn deaclear
-
 // let employname : string = 'erfan'
 // let ispressent : boolean = true;
-
 // let fruits: string[] = ["bannana","mango"]
-
 // let fruitsd : Array<string> = ["bannan","mango"]
-
-
 // if state 
-
 // let x: number= 10,y= 20;
-
 // if (x>y){
 //     console.log('x graet than y')
 // }else if ( x< y ){
 //     console.log('y graet than x')
-
 // }else{
 //     console.log('x=y')
 // }
-
-
 //  for Loop
-
 // for(let i=0; i<3;i++){
-
 // }
 // function sum(x:number,y:number){
 //     return x+y
-
 // }
-
 // class employname{
 //     empid: number;
 //     empname:string;
-
 //     constructor (empid: number, name : string){
 //         this.empId = empid;
 //     }
 // }
-
 // function sum (numb1,numb2){
 //     return numb1 + numb2 ;
 // }
 // console.log(sum(9,5));
 // console.log(sum(9,5));
 // console.log(sum(9,5));
-
-
-
-function welcome(message){
+function welcome(message) {
     return 'welcome to ' + message;
 }
-let user = 'erfan ';
+var user = 'erfan ';
 console.log(welcome(user));
